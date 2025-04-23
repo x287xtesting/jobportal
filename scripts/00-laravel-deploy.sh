564b81748d309c3c65f6510e0c25f74b507781f7
x287x-testing <115352423+x287x@users.noreply.github.com>
2025-04-23 23:32:20 +0300

    Update deploy script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -9,5 +9,5 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
-echo "Running migrations and seeding..."
-php artisan migrate:fresh --seed
+echo "Running migrations..."
+php artisan migrate --force
